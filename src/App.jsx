3447db55927b9d875871e6941fbc424abe6b8d07
Paomori <paomori18@gmail.com>
2024-03-26 19:12:31 -0300

    se agregar los componentes de react

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import SideBar from './components/SideBar';
+import './assets/css/app.css'
+import ContentWrapper from './components/ContentWrapper';
+
+function App() {
+
+  return (
+    <>
+    <div id="wrapper">
+    <SideBar/>
+    <ContentWrapper/>
+    </div>
+    </>
+  )
+}
+
+export default App
